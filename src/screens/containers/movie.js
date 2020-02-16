e7d317f50df543f71caf512674f31795a3db7731
Juan Carlos Garcia Esquivel <carlos.garcia.esquivel@live.com.mx>
2020-02-16 13:57:07 -0600

    animaciones

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -6,21 +6,34 @@ import Close from '../components/close';
 import { connect } from 'react-redux';
 import { REMOVE_SELECTED_MOVIE } from '../../../actions/actions';
 import Details from '../../videos/components/details';
+import { Animated } from 'react-native';
 
 class Movie extends React.Component {
+  state = {
+    opacity: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    Animated.timing(this.state.opacity, {
+      toValue: 1,
+      duration: 1000,
+    }).start();
+  }
   closeVideo = () => {
     this.props.dispatch(REMOVE_SELECTED_MOVIE());
   };
 
   render() {
     return (
-      <MovieLayout>
-        <Header>
-          <Close onPress={this.closeVideo} />
-        </Header>
-        <Player />
-        <Details {...this.props.selectedMovie} />
-      </MovieLayout>
+      <Animated.View style={{ flex: 1, opacity: this.state.opacity }}>
+        <MovieLayout>
+          <Header>
+            <Close onPress={this.closeVideo} />
+          </Header>
+          <Player />
+          <Details {...this.props.selectedMovie} />
+        </MovieLayout>
+      </Animated.View>
     );
   }
 }
